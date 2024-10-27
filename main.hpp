5faedace36c0b94a6f932d40fc580604383d8c59
Axelp1104 <a2pc0411@gmail.com>
2024-10-26 19:41:15 -0700

    1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,55 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+bool IsPrime (int);
+int getTwoValues (int, int);
+int getNextPrime (int);
+int getPrevPrime (int);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+
+int getTwoValues (int begin, int end)
+{
+    cin >> begin >> end;
+    if (begin > end)
+        cin >> begin >> end;
+    return begin;
+    return end;
+}
+bool IsPrime (int begin)
+{
+    if (begin <= 1)
+        return false;
+
+    for (int i = 2; i< begin; i++)
+        if (begin % i ==0)
+            return false;
+    return true;
+}
+int getNextPrime (int begin)
+{
+    int i,prime1;
+    for (i = begin+1; i <99; i++)
+        if (IsPrime(i))
+    return prime1;
+
+}
+int getPrevPrime (int end)
+{
+    int i,j,crt;
+    for (i = end-1; i>=1;i--){
+        for (j = 2; j<i; j++)
+            if (i%j==0)
+                crt++;
+        
+        if (crt==0)
+                if(i==0)
+                    break;
+                cout <<i;
+            crt =0;
+        return 0;
+    }
+
+}
+
+
+
